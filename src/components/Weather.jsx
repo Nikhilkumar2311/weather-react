0fc6725529c465687b6fd90856c2500a0f553f0e
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2025-01-20 18:58:05 +0530

    Updated

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -36,7 +36,9 @@ const Weather = () => {
       return;
     }
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=9700c6e52e3b48c9ac24e07063a0f075`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
       const response = await fetch(url);
       const data = await response.json();
 
